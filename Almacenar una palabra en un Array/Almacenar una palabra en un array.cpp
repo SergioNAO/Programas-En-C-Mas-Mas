752cd45fad9253ca7714bcf2d407b5c42f51b26c
SergioNAO <65801936+SergioNAO@users.noreply.github.com>
2022-10-20 12:16:50 -0500

    Add files via upload

---
+++ Almacenar una palabra en un Array/Almacenar una palabra en un array.cpp
@@ -0,0 +1,8 @@
+//Array de cadena (string o palabras)
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	char a[20];
+	printf("Digite su nombre: \n");gets(a);
+	printf("\nSu nombre es:%s\n",a);//el %s se utiliza para las cadenas o palabras
+}
