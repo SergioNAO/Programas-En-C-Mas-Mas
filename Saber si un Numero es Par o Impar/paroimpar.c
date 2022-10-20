2ac4f62cf950b14f8175dfcd9a59805f96b0dad6
SergioNAO <65801936+SergioNAO@users.noreply.github.com>
2022-10-20 12:11:32 -0500

    Add files via upload

---
+++ Saber si un Numero es Par o Impar/paroimpar.c
@@ -0,0 +1,15 @@
+//Par o impar
+
+#include<stdio.h>
+#include<conio.h>
+int a;
+int main(){
+ printf("Escribe un numero:  ");
+ scanf("%d",&a);
+if (a%2==0)
+printf("Es par");
+else
+printf("Es impar ");
+printf("\n");
+system("pause");
+}
