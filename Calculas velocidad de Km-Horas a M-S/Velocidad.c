ef0feb33dcf0e37be1fdfaf22bb87be8d84e6d0e
SergioNAO <65801936+SergioNAO@users.noreply.github.com>
2023-01-24 11:42:08 -0600

    Add files via upload

---
+++ Calculas velocidad de Km-Horas a M-S/Velocidad.c
@@ -0,0 +1,14 @@
+//Calcular Velocidad
+
+#include<stdio.h>
+
+int main(){
+float v,res=0,resu=0;
+printf("Ingresa la velocidad en km/h: ");
+scanf("%f",&v);
+res=v*1000/3600;
+
+printf("La Velocidad en m/s es:%f  ",res);
+
+
+}
